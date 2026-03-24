b241e6b773cde5d1de7289ad318ff4d462ea56f1
DanielPeretz213 <dp323010@gmail.com>
2026-03-24 12:03:06 +0200

    Add a definition socket

---
+++ client/src/services/socket.ts
@@ -0,0 +1,10 @@
+import { io, Socket } from "socket.io-client";
+
+const SOCKET_URL = "http://localhost:3000";
+
+export const socket : Socket = io(SOCKET_URL,{
+    autoConnect: true,
+    reconnection: true
+});
+
+export default socket;
